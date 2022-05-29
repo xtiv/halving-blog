c4af35a69a5f7b5339afdcdeb3b8267b9764af25
xtiv@gmail.com <xtiv@gmail.com>
2022-05-30 01:51:53 +0200

    Organizando /static/

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        chip_btc: "url('/assets/img/chip_btc.webp')",
-        halving_1: "url('/assets/img/halving-1.png')",
-        halving_2: "url('/assets/img/halving-2.png')",
-        donativo: "url('/assets/img/heart_money.png')",
-        btc_qr: "url('/assets/img/btc_address.jpeg')",
+        chip_btc: "url('/static/assets/img/chip_btc.webp')",
+        halving_1: "url('/static/assets/img/halving-1.png')",
+        halving_2: "url('/static/assets/img/halving-2.png')",
+        donativo: "url('/static/assets/img/heart_money.png')",
+        btc_qr: "url('/static/assets/img/btc_address.jpeg')",
       }
     },
   },
